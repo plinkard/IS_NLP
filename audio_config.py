c3a80e4d68adc6c5b6032c18632edee811da0c4c
plinkard <98417489+plinkard@users.noreply.github.com>
2022-04-02 16:27:54 -0400

    Delete audio_config.py

--- audio_config.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition as sr
-
-r = sr.Recognizer
-mic = sr.Microphone(2)
-
-with mic as source:
-  r.adjust_for_ambient_noise(source)
-  audio = r.listen(source)
-r.recongnize_google(audio)
