297e9f50a4ec22db170701a76b37b52b6c342f15
plinkard <98417489+plinkard@users.noreply.github.com>
2022-04-06 18:15:08 -0400

    Add files via upload

---
+++ final_proj/collect_audio_tester.py
@@ -0,0 +1,16 @@
+import random
+import time
+import speech_recognition as sr
+
+r=sr.Recognizer()
+
+#mic = sr.Microphone(1) #mic index in parenthesis
+
+proc_args = ['arecord', '-D' , 'plughw:1,0' , '--duration=10', 'test.wav']
+rec_proc = subprocess.Popen(proc_args, shell=False, preexec_fn=os.setsid)
+os.killpg(rec_proc.pid, signal.SIGTERM)
+
+with sr.WavFile("test.wav") as source:
+    r.adjust_for_ambient_noise(source)
+    audio=r.record(source)
+print(r.recognize_google(audio))
