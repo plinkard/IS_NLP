8e0fbd1a229a0ae26608534d07b52cf3ffa91467
plinkard <98417489+plinkard@users.noreply.github.com>
2022-04-28 17:11:11 -0400

    Update test_audio_1.py

--- final_proj/test_audio_1.py
+++ final_proj/test_audio_1.py
@@ -1,3 +1,4 @@
+
 import os
 import signal
 import subprocess
@@ -7,13 +8,10 @@ import speech_recognition as sr
 
 r=sr.Recognizer()
 
-#mic = sr.Microphone(1) #mic index in parenthesis
+mic = sr.Microphone(1) #mic index in parenthesis
 
-proc_args = ['arecord', '-D' , 'plughw:1,0' , '--duration=10', 'test.wav']
-rec_proc = subprocess.Popen(proc_args, shell=False, preexec_fn=os.setsid)
-os.killpg(rec_proc.pid, signal.SIGTERM)
 
-with sr.WavFile("test.wav") as source:
+with mic as source:
     r.adjust_for_ambient_noise(source)
-    audio=r.record(source)
+    audio=r.listen(source)
 print(r.recognize_google(audio))
